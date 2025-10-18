600aee3c20a59a9ae79bbb28a9a9c96899d75b4c
Jullie Anne A. Temporosa <jaalcala17@gmail.com>
2025-10-18 13:26:24 +0800

    Implement the Minimax

--- tictactoe-1.py
+++ tictactoe-1.py
@@ -32,9 +32,44 @@ def terminal(board):
     """True if the game is over (win or draw)."""
     return winner(board) is not None or not moves(board)
 
+def utility(board, me='O', opp='X'):
+    """Score terminal states from AI perspective: +1 win, -1 loss, 0 draw."""
+    w = winner(board)
+    if w == me:
+        return 1
+    elif w == opp:
+        return -1
+    else:
+        return 0  # draw or non-terminal (we only call this at terminal)
+
+def minimax(board, player, me='O', opp='X'):
+    """Return (best_value, best_move) assuming optimal play by both sides."""
+    if terminal(board):
+        return utility(board, me, opp), None
+
+    # Initialize best value depending on whose turn it is
+    best_val = -2 if player == me else 2
+    best_move = None
+
+    for m in moves(board):
+        b2 = board[:]
+        b2[m] = player
+        # Switch turn: if player was me, next is opp; else me
+        next_player = opp if player == me else me
+        val, _ = minimax(b2, next_player, me, opp)
+
+        if player == me and val > best_val:
+            best_val, best_move = val, m
+        elif player == opp and val < best_val:
+            best_val, best_move = val, m
+
+    return best_val, best_move
+
 if __name__ == "__main__":
-    b = ['X','X','X',' ',' ',' ',' ',' ',' ']
-    print_board(b)
-    print("Winner should be X ->", winner(b))
-    print("Moves available ->", moves(b))
-    print("Terminal? ->", terminal(b))
+    # AI = 'O' to move, human = 'X'
+    board = ['X','O','X',
+             'O','X',' ',
+             ' ','O',' ']
+    print_board(board)
+    val, move = minimax(board, player='O', me='O', opp='X')
+    print("Minimax suggests move:", move, "with value:", val)
